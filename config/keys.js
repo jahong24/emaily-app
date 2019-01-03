140ecb39a00886205e5f8a2b55ab6609ec8d4806
Jonathan <jahong24@gmail.com>
2019-01-02 19:49:11 -0500

    Generate client code using create react app
    Added Redux and React Router

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
   googleClientID:
     "247605312030-v43lkugkjf4tgqi3o0f05a973rambj9m.apps.googleusercontent.com",
-  googleClientSecret: "YNNmuazS173-1olNK5TilXiK"
+  googleClientSecret: "YNNmuazS173-1olNK5TilXiK",
+  mongoURI: "mongodb://admin:password1@ds139934.mlab.com:39934/emaily-jonh-dev",
+  cookieKey: "oiwejasdfasdf"
 };
