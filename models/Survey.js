f66e562a339b57dc9a80d5d079e30e54444ff429
Hong, Jonathan <jonathan.hong@optum.com>
2019-01-07 16:44:45 -0500

    Survey model

--- models/Survey.js
+++ models/Survey.js
@@ -8,7 +8,10 @@ const surveySchema = new Schema({
   subject: String, // Subject of email
   recipients: [RecipientSchema], // Array of RecipientSchema records
   yes: { type: Number, default: 0 }, // Count of 'yes' responses
-  no: { type: Number, default: 0 } // Count of 'no' responses
+  no: { type: Number, default: 0 }, // Count of 'no' responses
+  _user: { type: Schema.Types.ObjectId, ref: "User" },
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model("surveys", surveySchema);
