e66c13e2f01cfda09f48246920f78d0c6a98860f
Jonathan <jahong24@gmail.com>
2019-01-09 21:45:07 -0500

    Email survey feature

--- config/keys.js
+++ config/keys.js
@@ -5,5 +5,8 @@ module.exports = {
   mongoURI: "mongodb://admin:password1@ds139934.mlab.com:39934/emaily-jonh-dev",
   cookieKey: "oiwejasdfasdf",
   stripePublishableKey: "pk_test_J88TRC8Y1gkOWB5NOaScqQmA",
-  stripeSecretKey: "sk_test_c7AS9ayicGAN4rS48mT4qQB3"
+  stripeSecretKey: "sk_test_c7AS9ayicGAN4rS48mT4qQB3",
+  sendGridKey:
+    "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",
+  redirectDomain: "http://localhost:3000"
 };
---
+++ services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,21 @@
+const keys = require("../../config/keys");
+
+module.exports = survey => {
+  return `
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <h3>I'd like your input!</h3>
+          <p>Please answer the following question:</p>
+          <p>${survey.body}</p>
+        <div>
+        <div>
+          <a href="${keys.redirectDomain}/api/thanks">Yes</a>
+        </div>
+        <div>
+          <a href="${keys.redirectDomain}/api/thanks">No</a>
+        </div>
+      </body>
+    </html>
+  `;
+};
